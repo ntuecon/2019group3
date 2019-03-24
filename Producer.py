b59e1962ea518ee374b62919d33341864c307571
ricardmilabou <48195266+ricardmilabou@users.noreply.github.com>
2019-03-24 23:57:59 +0800

    Create Producer.py

---
+++ Producer.py
@@ -0,0 +1,18 @@
+"""
+
+
+# Define the variables for the producer, to simulate the production of different goods in the economy
+  def __init__(self,c,g,f):
+       # c: Number of consumers in the economy
+       # g: Number of goods produced in the economy
+       # f: Number of factors used in the production
+       
+       self.c = c
+       self.g = g
+       self.f = f
+       
+       #Porduction Technologies 
+       # r:factor f used in the procudction of good g
+       # x: Aggregate amount of good 
+       
+
