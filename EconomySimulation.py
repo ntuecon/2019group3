23b2952802765024d4e86d035aecd6367e48e0f2
neni45902 <48184762+neni45902@users.noreply.github.com>
2019-03-28 09:03:15 +0100

    Create EconomySimulation.py
    This is the blueprint on what will need to happen in the EconomySimulation class. The EconomySimulation class shall act as controller (when considering the MVC model). And the user input as well as modelled objects (Economy, Producer, Consumer) will be created here.

---
+++ EconomySimulation.py
@@ -0,0 +1,18 @@
+import Economy
+import Consumer
+import Producer
+
+class EconomySimulaton (object):
+
+    #number of goods and factors will be defined
+
+    #consumer objects will be created
+
+    #producer objects will be created
+
+    #economy object will be created (giving the object information on number
+    #goods and factors, arrays of the created consumers and producers
+
+    #findEquilibrium() method will be called to let the equilibrium be calculated
+
+    
