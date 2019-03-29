b43fb3431b033cc61e7ec8a7e9d7dad164dca3dd
sabrinadine <48429152+sabrinadine@users.noreply.github.com>
2019-03-29 15:19:06 +0800

    Update Economy2.py

--- Economy2.py
+++ Economy2.py
@@ -22,6 +22,8 @@ class market_clearing (object):
   #now we create the equilibrium method
   
   def obective1 (Xhg, Xg):
+    def maxUtility(self, pi, p, r, guess):
+    def profit
     return (Xhg - Xg) **2
   
   #now we make a guess
@@ -30,5 +32,21 @@ class market_clearing (object):
   
   print (obective1 (X))
   
-  sol = minimize (objective
+  sol = minimize (objective)
     
+    def obective2 (Vhg, Vg):
+    def maxUtility(self, pi, p, r, guess):
+    def profit
+    return (Vhg - Vg) **2
+  
+  #Now we compute the wellfare function as the outcome of this allocation
+  # we use the maxUtility method 
+  
+  for h in consumer:
+    maxUtility = 0
+    maxUtility += maxUtility
+    return maxUtility
+  
+  
+  
+  
