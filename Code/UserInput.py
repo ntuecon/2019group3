944bc8b111b0f34e2572180fa451f530c17fe1f9
neni45902 <48184762+neni45902@users.noreply.github.com>
2019-04-21 09:56:30 +0200

    Create UserInput.py
    This file will contain the communication with the user. It will be extended by further functions when we know the role of the government and know what options the user will have to let the government intervene in the economy as well as which numbers we will measure to give the user an level of equality we have in a specific economy.

---
+++ Code/UserInput.py
@@ -0,0 +1,23 @@
+"""
+@author Michelle Pfister
+@version 1.0
+
+"""
+
+class UserInput (object):
+  
+  def introduction (self):
+  """ introduction will be used when beginning the economy simulation as a user to explain how the economy simuation works """
+  
+    print "Welcome to your Economy!"
+    print "In the following you will be able to specify the parameters to your economy and be able to simulate your economy! "
+    print "But first a few explanations about this economy simulation. Your economy has three main actors: consumers, producers and the government. "
+    
+    
+  def inputValues (self):
+    """ inputValues will be used to describe to the user which parameters he has to input to define the economy (e.g. number of goods
+        and factors, number of consumers and producers, link between consumer and producers). """
+        
+        
+  
+  
