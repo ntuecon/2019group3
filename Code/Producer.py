4f5b93eca14f4f83099d6a21c09421c2da68e394
ricardmilabou <48195266+ricardmilabou@users.noreply.github.com>
2019-04-22 12:13:03 +0800

    Update Producer.py

--- Code/Producer.py
+++ Code/Producer.py
@@ -1,18 +1,44 @@
-"""
+
+import numpy
+from scipy.optimize import minimize
 
 
-# Define the variables for the producer, to simulate the production of different goods in the economy
-  def __init__(self,c,g,f):
-       # c: Number of consumers in the economy
-       # g: Number of goods produced in the economy
-       # f: Number of factors used in the production
-       
-       self.c = c
-       self.g = g
-       self.f = f
-       
-       #Porduction Technologies 
-       # r:factor f used in the procudction of good g
-       # x: Aggregate amount of good 
+class Producer (object):
+    """ This class models the producer """
+
+     def __init__ (self,noProducer, noOfGoods, noOfFactors, parameterDict):
+        """ This is the constructor for the producer class, it provides the consumer object with all the parameters
+            needed to calculate the profit, as well as the link to which producers profit. 
+            noOfProducer : number that indicates which producers profit will go to this consumer, 0 if no producers profit goes
+            to this consumer
+            noOfGoods, noOfFactors : number of goods and factors in this economy 
+            parameterDict : dictonary that contains all parameters that define the production function ()  """
+        
+        self.noProducer = noProducer
+        self.noOfGoods = noOfGo
+        self.noOfFactors = noOfFactors
+        self.parameterDict = parameterDict
+
+#constraint
+        def prodtFct (r)
+        """ This function calculates the production function """
+
+        
+         #seperate good array (Xhg) from factor array (Vhf) goods come first
+        Xhg = numpy.array(inputList[0:self.noOfGoods])
+        Vhf = numpy.array(inputList[self.noOfGoods: self.noOfGoods+ self.noOfFactors])
+    
+
+
+#objective
+        def profitFct (Xhg, Vhg)
+        """ In this section we define the profit function of producers"""
+        
+
+#maximization
+        def maxProfit (r,p)
+
+            """
+                    """
        
 
