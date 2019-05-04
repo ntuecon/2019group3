6aaeb964c6337fa125be298010fe03ba93e12d6f
neni45902 <48184762+neni45902@users.noreply.github.com>
2019-05-04 06:13:44 +0200

    Update EconomySimulation.py
    Code added. Consumer and Producer can now be defined by user and created.

--- Code/EconomySimulation.py
+++ Code/EconomySimulation.py
@@ -4,24 +4,41 @@
 
 """
 
-import Economy
-import Consumer
-import Producer
-import UserInput
-
-class EconomySimulaton (object):
-
-    #input of data by the user has to happen using functions of the class UserInput
-    
-    #number of goods and factors will be defined
-    
-    #consumer objects will be created
-
-    #producer objects will be created
-
-    #economy object will be created (giving the object information on number
-    #goods and factors, arrays of the created consumers and producers
-
-    #findEquilibrium() method will be called to let the equilibrium be calculated
+import numpy
+from Consumer import Consumer
+from Producer import Producer
+from UserInput import UserInput
+#from Economy import Economy
 
     
+#explanation
+user = UserInput()
+user.introduction()
+
+#input of data by the user has to happen using functions of the class UserInput
+value = user.inputValues()
+    
+#consumer objects will be created
+
+consumerArray = [Consumer]*value.noOfConsumers
+for c in range(0,len(consumerArray)):
+    consumerArray[c] = Consumer(value.prodOfCon[c],value.noOfGoods,value.noOfFactors,value.parameterCon[c])
+    #NOOFPRODUCER HAS TO BE CHANGED
+
+print "Consumer created"
+    
+#producer objects will be created
+producerArray = [Producer]*value.noOfProducers
+for p in range(0,len(producerArray)):
+    producerArray[p] = Producer(value.noOfProducers, value.noOfGoods, value.noOfFactors, value.parameterProd[p], value.goodProd[p])
+
+print "Producer created"
+    
+
+#economy object will be created (giving the object information on number
+
+#economy = Economy ( #HAS TO BE CREATE 
+#goods and factors, arrays of the created consumers and producers
+
+#findEquilibrium() method will be called to let the equilibrium be calculated
+
