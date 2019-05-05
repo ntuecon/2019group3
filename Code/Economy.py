7ce83beea7b4457462d3f1d2a35546031ffcc44e
neni45902 <48184762+neni45902@users.noreply.github.com>
2019-05-05 16:09:05 +0200

    Update Economy.py
    fixes

--- Code/Economy.py
+++ Code/Economy.py
@@ -1,83 +1,71 @@
 import numpy as np
-from scipy.optimize import minimize, fmin
-#import matplotlib as plt
+from scipy.optimize import minimize
 import array
-import consumer as con
-#import producer as prod
+from Consumer import Consumer as con
+from Producer import Producer as prod
 
 class Economy(object):
 	
-"""the object is to find factor and goods prices 
-that make the difference of goods provided by the producer and goods demanded by the consumer,
-and the factors provided by the consumer and factors demanded by the producer to be 0"""
 
-  def __init__ (self, askCon, askProd,noOfGoods,noOfFactos):
-    self.askCon = askCon
-    self.askProd = askProd
-    self.noOfGoods = noOfGoods
-    self.noOfFactors = noOfFactors
-    #self.noOfCon = NoOfCon
-    #self.noOfProd = NoOfProd
- 
-  def objective (self, inputList, no): 
-		
- """In order to obtain maximized values from the above mentioned classes,
-    prices for each good and factors need to be provided.
-    The consumer also needs to know about the profit""" 
-  
-  
-  p = numpy.array(inputList[0:self.noOfGoods]) 
-  r = numpy.array(inputList[self.noOfGoods: self.noOfGoods+ self.noOfFactors]) 
-  prodProfit = numpy.empty(len(askProd))
+    def __init__ (self, askCon, askProd, noOfGoods, noOfFactors):
+        self.askCon = askCon
+        self.askProd = askProd
+        self.goods = goods
+        self.factors = factors
+    
 
- """ We start with the producer first in order to obtain the profit we will need for the consumer"""
-  
-  #Producer problem
-	
-  sumProdGoods = 0 
-  sumProdFactors = 0 
-        
-    for i in self.askProd :
-      j = 0 
-      answerProd = self.i.maxProfit (p,r) 
-      sumProdGoods += answerProd[0:self.noOfGoods] 
-      sumProdFactors += answerProd[self.noOfGoods: self.noOfGoods+ self.noOfFactors] 
-      prodProfit[j] = answerProd [noOfGoods+noOfFactors]
-      j += 1 
-		
-	 #Redistribution of profit
-	 conProfit = numpy.empty(len(askCon)) 
-	 for i in self.askCon :
-	   j = 0 
-	   if i.noOfProd == 0 : 
-	     conProfit[j] = 0
-           else :
-	     conProfit[j] = prodProfit[i.noOfProd-1] 
-		 j += 1
+    def objective (self, inputList, no):
+        p = numpy.array(inputList[0:self.noOfGoods])
+        r = numpy.array(inputList[self.noOfGoods: self.noOfGoods+ self.noOfFactors])
+        prodProfit = numpy.empty(len(askProd))
 
- #Consumer Problem
+        #Producer problem
+	sumProd = 0  
 
- sumConGoods = 0 
- sumConFactors = 0 
-	 
-    for i in self.askCon:
-	
-	answerCon =self.i.maxUtility (conProfit[j],p,r )
-	sumConGoods += answerCon [0:self.noOfGoods]
-	sumConGoods += answerCon[self.noOfGoods: self.noOfGoods+ self.noOfFactors]
+        for i in self.askProd :
+            j = 0
+            answerProd = self.i.maxProfit (p,r)
+            sumProd += answerProd[no]
+            prodProfit[j] = answerProd [noOfGoods+noOfFactors]
+            j += 1
 
-	return sqrt(sumConGoods - sumProdGoods) + sqrt(sumConFactors - sumProdFactors)
+        #Redistribution of profit
+        conProfit = numpy.empty(len(askCon))
+        for i in self.askCon :
+            j = 0
+            if i.noOfProd == 0 :
+                conProfit[j] = 0
+            else :
+                conProfit[j] = prodProfit[i.noOfProd-1]
+            j += 1
+
+        #Consumer Problem
+
+        sumCon = 0
+
+        j = 0
+        for i in self.askCon:
+            answerCon =self.i.maxUtility (conProfit[j],p,r )
+            sumCon += answerCon [no]
+            j += 1
+            return sqrt(sumCon - sumProd) 
               
- 
-# Find prices and wages that minimize to 0 this difference
-#Try or guesses
-p = numpy.ones(inputList[0:self.noOfGoods])
-r = numpy.ones(inputList[0:self.noOfGoods])
-print objective (p,r)
 
-sol = minimize (objective, p,r, method ='SLSQP')
+    def findEquilibrium(self):
+        # Find prices and wages that minimize to 0 this difference
+        #Try or guesses
+        guess = numpy.empty(self.noOfGoods + self.noOfFactors)
+        for no in range (0, self.noOfGoods+self.noOfFactors):
+            sol = minimize (self.objective,guess,args = (no), method ='SLSQP')
+            result = self.objective(sol.x,no)
+            if result == 0:
+                print "Good / Factor No: " + str(no) + " market is cleared"
+            else :
+                print "ERROR"
 
-print (sol) 
+
   
+
+
 
 
