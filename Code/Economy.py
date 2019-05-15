977e6cef46e29c4375134cdbb8fb8797fa8b285f
neni45902 <48184762+neni45902@users.noreply.github.com>
2019-05-15 15:06:13 +0200

    Update Economy.py
    Change in objective function

--- Code/Economy.py
+++ Code/Economy.py
@@ -15,18 +15,22 @@ class Economy(object):
         self.noOfFactors = noOfFactors
     
 
-    def objective (self, inputList, no):
+    def objective (self, inputList):
         p = numpy.array(inputList[0:self.noOfGoods])
         r = numpy.array(inputList[self.noOfGoods: self.noOfGoods+ self.noOfFactors])
         prodProfit = numpy.empty(len(self.askProd))
 
-        #Producer problem
-        sumProd = 0  
+        consumedArray = numpy.empty(self.noOfGoods+self.noOfFactors)
+        producedArray = numpy.empty(self.noOfGoods+self.noOfFactors)
+
+        
+        #Producer problem 
 
         for i in self.askProd :
             j = 0
             answerProd = i.maxProfit (p,r)
-            sumProd += answerProd[no]
+            for no in  range (0, self.noOfGoods+self.noOfFactors):
+                producedArray[no] += answerProd[no]
             prodProfit[j] = answerProd [self.noOfGoods+self.noOfFactors]
             j += 1
 
@@ -47,23 +51,41 @@ class Economy(object):
         j = 0
         for i in self.askCon:
             answerCon =i.maxUtility (conProfit[j],p,r )
-            sumCon += answerCon [no]
+            for no in  range (0, self.noOfGoods+self.noOfFactors):
+                consumedArray[no] += answerCon [no]
             j += 1
 
-        return pow(sumCon,2) - pow(sumProd,2) 
-              
 
+        result = 0
+
+        for i in range (0, self.noOfGoods+self.noOfFactors):
+            result += pow(consumedArray[no]-producedArray[no],2)
+
+        return result
+              
+    #positivity condition
+    def constraint(self, inputList,no):
+        return inputList[no]
+        
     def findEquilibrium(self):
         # Find prices and wages that minimize to 0 this difference
         #Try or guesses
         guess = numpy.empty(self.noOfGoods + self.noOfFactors)
+        #positivity constraints
+        
+        constraintPos = [{}]*(self.noOfGoods + self.noOfFactors)
         for no in range (0, self.noOfGoods+self.noOfFactors):
-            sol = minimize (self.objective,guess,args = (no), method ='SLSQP')
-            result = self.objective(sol.x,no)
-            if result == 0:
-                print "Good / Factor No: " + str(no) + " market is cleared"
-            else :
-                print "ERROR"
+            con = {'type' : 'ineq', 'fun' : self.constraint, 'args' :[no]}
+            print str(no) + "NO"
+            constraintPos[no] = con
+            
+        
+        sol = minimize (self.objective,guess,args = ( ) , method ='SLSQP', constraints = constraintPos)
+        
 
         return sol.x
+
+
+  
+
 
