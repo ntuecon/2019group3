86a9a3643250fc13109992200bf6986964ffcb68
sabrinadine <48429152+sabrinadine@users.noreply.github.com>
2019-06-03 15:27:31 +0800

    Added descriptions of parameters

--- Code/UserInput.py
+++ Code/UserInput.py
@@ -16,6 +16,11 @@ class UserInput (object):
         print "Welcome to your Economy!"
         print "In the following you will be able to specify the parameters to your economy and be able to simulate your economy."
         print "But first a few explanations about this economy simulation. Your economy has three main actors: consumers, producers and the government. "
+        print "The producer's profit function depends on the Elasticity of the product, that is xi or how easy is it to producer more of this product and Psi, the efficiency of that product " 
+        print "The consumer has an utility function, that consists of the utility of the goods and the disutility of the factors provided"
+        print "Alpha is the utility weight, and the alphas between different goods should add up to 1. Beta is the disutility weight of the factors provided"
+        print "Sigma is the elasticity of substitution between goods."
+        print "Gamma is the elasticity of consumption and Theta de elasticity of Factors. They are needed so the functions are well behaved and the Consumer doen't have unlimited wants."
  
     
     def inputValues (self):
@@ -37,8 +42,8 @@ class UserInput (object):
         for i in range(0,noOfProducers) :
             print "You define producer " + str(i+1) + " now"
             goodProducer = int(raw_input("Which product should this producer produce?"))
-            xi = float(raw_input ( "What do you want xi to be?"))
-            psi = float(raw_input ( "What do you want psi to be?"))
+            xi = float(raw_input ( "What do you want xi, the elasticity of the product, to be? Needs to be 0 and 1. We recommend 0.5, if you the input is closer to 0, then it becomes very elastic, and closer or above 1, it becomes very unelastic")
+            psi = float(raw_input ( "What do you want psi, the technology or effiency of the product, to be? "))
             producerDict = {"xi" : xi, "psi" : psi}
             goodProd[i] = goodProducer
             parameterProd[i] = producerDict
@@ -54,14 +59,16 @@ class UserInput (object):
             prodOfConsumer = int(raw_input("Which producers profit goes to this consumer?"))
             alphaArray = numpy.empty(noOfGoods)
             for j in range(0, noOfGoods):
-                alpha = float(raw_input( "What do you want alpha to be? Must be greater than 0, unless this good is a bad" ))
+                alpha = float(raw_input( "What do you want alpha, the weight of utility, to be? Must be greater than 0, unless this good is a bad and the alphas of different goods must add up to 1" ))
                 alphaArray[j] = alpha
-            beta = float(raw_input ( "What do you want beta to be? Must be greater than 0, unless the consumer likes to give away this factor" ))
-            gamma = float(raw_input ( "What do you want gamma to be? Must be greater than 0"))
-            sigma = float(raw_input ( "What do you want sigma to be? Must be greater than 0, and LESS than 1" ))
+            beta = float(raw_input ( "What do you want beta, the weight of disutility, to be? Must be greater than 0, unless the consumer likes to give away this factor" ))
+            gamma = float(raw_input ( "What do you want gamma,the elasticity of consumption, to be? Must be greater than 0 and less than 1. We recommend 0.5, if the chosen input is closer to 0, then it becomes very elastic, and closer or above 1, it becomes very unelastic"))
+            sigma = float(raw_input ( "What do you want sigma, elasticity of substitution between goods, to be? Must be greater than 0, and LESS than 1. It's the elasticity between goods, if just one good, set it equal to 0" ))
+            #If sigma is equal to 1, we could code a coub douglas function, it does not need gama and theta, because it is already well behaved.
+            # It is ln (multiplier of xi to alphai), but we used the function of the sum of alpha i of ln (xi)
             thetaArray = numpy.empty(noOfFactors)
             for k in range (0, noOfFactors):
-                theta = float(raw_input ( "What do you want theta to be? Must be greater than -1"))
+                theta = float(raw_input ( "What do you want theta, the elasticity of factors, to be? Must be greater than -1"))
                 thetaArray[k] = theta
 
             consumerDict = {"alpha" : alphaArray, "beta" : beta, "gamma" : gamma, "sigma" : sigma, "theta" : thetaArray}
